9f2ffd1b9b1e5363949a22b1b6d31fa4378dfc7a
aperiodic <dlp@aperiodic.org>
2015-03-06 22:28:39 -0800

    Add project.clj w/ring, compojure, quil deps.
    Also add lein's default .gitignore.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject many-worlds "0.1.0-SNAPSHOT"
+  :description "Interactive, parallel state space exploration for quil sketches."
+  :url "https://github.com/aperiodic/many-worlds"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.2"]
+                 [ring "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [org.clojars.aperiodic/quil "1.6.1"]])
