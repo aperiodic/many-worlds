7a2837635e93d32022465e6c11160c37b3e30973
aperiodic <dlp@aperiodic.org>
2015-04-07 18:52:50 -0700

    Add cljs hello world.
    Add clojurescript as a dependency, lein-cljsbuild as a plugin, a "Hello
    World!" cljs namespace and an index.html that loads the clojurescript
    properly.

--- project.clj
+++ project.clj
@@ -4,9 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3126"]
                  [compojure "1.3.2"]
                  [ring "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
                  [org.clojars.aperiodic/quil "1.6.1"]
                  [aperiodic/qutils "0.1.0-SNAPSHOT"]]
-  :source-paths ["src/clj"])
+  :source-paths ["src/clj"]
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.4"]]}}
+  :cljsbuild {
+    :builds {:dev {:source-paths ["src/cljs"]
+                   :compiler {:output-to "resources/public/js/compiled/main.js"
+                              :output-dir "resources/public/js/compiled/out"
+                              :optimizations :none
+                              :source-map true}}}})
---
+++ src/cljs/many_worlds/console.cljs
@@ -0,0 +1,5 @@
+(ns many-worlds.console)
+
+(enable-console-print!)
+
+(println "Hello from many-worlds.console!")
