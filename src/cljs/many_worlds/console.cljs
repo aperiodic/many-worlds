04f149b7a455ac68c9a72834baeab8e725702891
aperiodic <dlp@aperiodic.org>
2015-04-11 13:00:46 -0700

    Add time controls component.
    Allows user to fast-forward & rewind, skip ahead & back, and reset the
    speed to 2x. Doesn't yet actually update the time.

--- src/cljs/many_worlds/console.cljs
+++ src/cljs/many_worlds/console.cljs
@@ -17,7 +17,9 @@
 ;; State
 ;;
 
-(defonce !state (atom {:worlds []}))
+(defonce !state
+  (atom {:worlds []
+         :time {:t 0, :speed 2}}))
 
 ;;
 ;; Components
@@ -30,7 +32,7 @@
   (reify
     om/IRender
     (render [_]
-      (let [t (:t state 0)]
+      (let [t (get-in state [:time :t] 0)]
         (html
           [:div.worlds
            (for [world (:worlds state)]
@@ -65,6 +67,38 @@
          [:button {:on-click #(add-world state owner)}
           "Add World"]]))))
 
+(def delta-t
+  "In order to calculate how far to jump around when skipping, multiply this
+  delta by the current speed. With the default speed of 1 the jump will be just
+  this value, in seconds."
+  0.25)
+
+(defn time-component
+  [{:keys [t speed] :as time} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [reset-speed #(om/transact! time :speed (constantly 2))
+            ff #(om/transact! time :speed (fn [speed] (+ speed 0.5)))
+            fb #(om/transact! time :speed (fn [speed] (max (- speed 0.5) 1.0)))
+            skip-back #(om/transact! time
+                                     (fn [{:keys [speed] :as time}]
+                                       (-> time
+                                         (update-in [:t] - (* speed jump-delta-t))
+                                         (update-in [:t] max 0))))
+            skip-ahead #(om/transact! time
+                                      (fn [{:keys [speed] :as time}]
+                                        (update-in time [:t] + (* speed delta-t))))]
+        (html
+          [:div#time
+           [:button.skip-back {:on-click skip-back} "⏮"]
+           [:button.fast-backward {:on-click fb} "⏪"]
+           [:span.time-display (str "t: " (.toFixed t 1))]
+           [:span.speed-display (str "(" (.toFixed speed 1) "x)")]
+           [:button.fast-forward {:on-click ff} "⏩"]
+           [:button.skip-ahead {:on-click skip-ahead} "⏭"]
+           [:button.reset-speed  {:on-click reset-speed} "speed = 2x"]])))))
+
 ;;
 ;; Om App
 ;;
@@ -76,6 +110,7 @@
       (render [_]
         (html
           [:div#many-worlds-console
+           (om/build time-component (:time state))
            (om/build previews-component state)
            (om/build add-world-component state)]))))
 
